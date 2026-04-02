ec0b292660d0fd3128f689649b1676795ddb6c87
bsraghavan93 <bsraghavan93@gmail.com>
2026-04-02 04:28:10 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Hero from "./components/Hero";
+import Sections from "./components/Sections";
+import "./index.css";
+
+export default function App() {
+  return (
+    <>
+      <Hero />
+      <Sections />
+    </>
+  );
+}
